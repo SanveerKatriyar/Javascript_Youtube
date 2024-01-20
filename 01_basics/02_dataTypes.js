38fb96bed94c09d4eade8aa9e4a6e5908c35cace
SanveerKatriyar <89511790+SanveerKatriyar@users.noreply.github.com>
2024-01-20 21:38:57 +0000

    DataTpes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs , its working on browser
+
+console.log(3+3)
+
+let name = "sanveer"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null => standalone value, Empty
+// undefine
+// symbol => unique
+
+// Object
+
+console.log(typeof null) // null is object
+console.log(typeof undefined) // unidentified
