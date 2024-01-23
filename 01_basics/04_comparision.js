ef917704014872bc65af49d715bfc0426c28dc25
SanveerKatriyar <89511790+SanveerKatriyar@users.noreply.github.com>
2024-01-23 21:21:37 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // wrong output compare to youtube
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null >= 0) //conversion issue
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+
+// strict check ==== 
+console.log("2" === 2) // checks datatype also
+console.log("2" == 2) //conversion done only
+
+
+
